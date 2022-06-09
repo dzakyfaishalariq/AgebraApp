ff245fd6be90e8a49769f302af6784bb4c826eba
Dzaky_Faishalariq003 <dzakyfaishalariq@gmail.com>
2022-06-10 03:50:13 +0700

    Algebra aplikasi

---
+++ resources/views/mytemplate/main.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" href="https://imgs.search.brave.com/vQuIua28MDZljNUfUSVHIpFbd72g5Um85E1469i4B3s/rs:fit:497:225:1/g:ce/aHR0cHM6Ly90c2U0/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5Y/WlJ1TXNZY2kyc2Nz/bldwUjA3NDFBSGFI/RSZwaWQ9QXBp" type="image/x-icon">
+    
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        @yield('conten')
+    </div>
+</body>
+</html>
